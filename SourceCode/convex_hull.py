6c957688d9651509dd7c44216d63efe5bae78efd
Yanxi_Li <LiYanxi1024@gmail.com>
2023-10-30 22:35:53 -0400

    update structure

--- SourceCode/convex_hull.py
+++ SourceCode/convex_hull.py
@@ -1,15 +1,16 @@
 class Solution:
+    # Divide operation, to divide a point set group into groups of 3 or less
     def divide(self, convex_hull, start: int, end: int):
         if end - start < 3:
             return convex_hull[start:end + 1]
         mid = int((start + end) / 2)
-        left_part = self.divide(convex_hull, start, mid)
-        right_part = self.divide(convex_hull, mid + 1, end)
+        left_part = self.divide(convex_hull, start, mid) #Recursively find the result of the left part
+        right_part = self.divide(convex_hull, mid + 1, end) #Recursively find the result of the right part
         return self.combine(left_part, right_part)
     
+    # Conquer operation, Combines the left and right convex hulls into a single convex hull.
     def combine(self, left_part, right_part):
         convex_hull = left_part + right_part
-        # print(f"{left_part};;{right_part}")
         
         def crossPro(p, o, q):
             x1, y1 = p
@@ -19,6 +20,7 @@ class Solution:
         
         upper_boundery, lower_boundery  = [], []
         
+        # Find the upper and lower bounds of the merged points
         for point in convex_hull:
             while len(upper_boundery) >= 2 and crossPro(upper_boundery[-2], upper_boundery[-1], point) > 0:
                 upper_boundery.pop()
@@ -27,12 +29,11 @@ class Solution:
             
             lower_boundery.append(tuple(point))
             upper_boundery.append(tuple(point))
-        # print(upper_boundery)
-        # print(lower_boundery)
 
+        # Remove duplicate points in the upper and lower boundaries to ensure that the same point appears only once in the upper and lower boundaries
         de_st = 0
         e1 = e2 = 0
-        while 1:
+        while 1: #Remove duplicate points near the left endpoint
             if e1 >= len(upper_boundery) or e2 >= len(lower_boundery):
                 de_st = e1
                 break
@@ -43,7 +44,7 @@ class Solution:
             e2 += 1
         de_en = e1 = len(upper_boundery) - 1
         e2 = len(lower_boundery) - 1
-        while 1:
+        while 1: #Remove duplicate points near the right endpoint
             if e1 < 0:
                 de_en = -1
                 break
@@ -56,14 +57,13 @@ class Solution:
             e1 -= 1
             e2 -= 1
 
-        # print(de_st)
-        # print(de_en)
         if de_en < de_st:
             upper_boundery = []
         else:
-            upper_boundery = upper_boundery[de_st:de_en + 1]
-        # print(upper_boundery)
+            upper_boundery = upper_boundery[de_st:de_en + 1] # remove the duplicate points
 
+        # Merge the upper and lower boundaries into a new convex hull, and ensure that the merged points are still sorted from smallest to largest
+        # The time complexity of this operation is O(n), with n being the number of points in the current merged convex hull
         e1 = e2 = 0
         res = []
         while e1 < len(lower_boundery) and e2 < len(upper_boundery):
@@ -88,12 +88,13 @@ class Solution:
         res = list(map(list, res))
         # print(res)
         return res
-            
-    def convexSolution(self, convex_hull):
-        convex_hull = sorted(convex_hull)
-        # print(convex_hull)
-        res = self.divide(convex_hull, 0, len(convex_hull) - 1)
-        print(res)
+    
+    # The entry function
+    def convexSolution(self, points):
+        points = sorted(points) #Sort the input points from smallest to largest
+        
+        convex_hull = self.divide(points, 0, len(points) - 1) # Call the divide-and-conquer algorithm to get the result
+        print(convex_hull) # print the result
 
 def main():
     solution = Solution()
